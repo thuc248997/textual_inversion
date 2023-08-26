65d2ebe9cac4aafd1d0bd09512636b01cdecaaaf
thuc248997 <thuc248997@gmail.com>
2023-08-26 21:25:37 +0700

    change num_vector to 2

--- run_train.sh
+++ run_train.sh
@@ -4,4 +4,4 @@
 #         --config /home/cntt/haohq/stable_diffusion/textual_inversion/configs/default.yaml
 
 python components/train/train.py \
-        --config /home/cntt/haohq/stable_diffusion/textual_inversion/configs/only_a_template.yaml
+        --config /home/cntt/haohq/stable_diffusion/textual_inversion/configs/no_use_template.yaml
